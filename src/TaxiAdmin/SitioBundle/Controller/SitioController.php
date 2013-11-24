72762dedb69665b58630e925192f8169500c91d9
Brus <brunovierag@gmail.com>
2013-11-24 17:58:23 -0200

    agregando e imprimiendo el formulario de registro

--- src/TaxiAdmin/SitioBundle/Controller/SitioController.php
+++ src/TaxiAdmin/SitioBundle/Controller/SitioController.php
@@ -2,12 +2,34 @@
 
 namespace TaxiAdmin\SitioBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+use TaxiAdmin\UsuarioBundle\Entity\Usuario;
+use TaxiAdmin\UsuarioBundle\Form\UsuarioType;
+
+/**
+ * Sitio controller.
+ *
+ * @Route("/")
+ */
 class SitioController extends Controller {
 
+	/**
+     * Show the homepage 
+     *
+     * @Route("/", name="siti_ohome")
+     * @Method("GET")
+     * @Template("")
+     */
 	public function homeAction() {
-		return $this->render('TaxiAdminSitioBundle:Sitio:home.html.twig');
+		$form = $this->createForm(new UsuarioType(), new Usuario);
+		return array(
+				'formUsuario'   => $form->createView(),
+			);
 	}
 	
 }
