ae08321dd5ff9c723f9937df90b9b07ff5daa0d7
Brus <brunovierag@gmail.com>
2013-11-24 17:58:32 -0200

    ajustando el formulario de registro

---
+++ src/TaxiAdmin/UsuarioBundle/Form/UsuarioType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace TaxiAdmin\UsuarioBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class UsuarioType extends AbstractType {
+
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder
+            ->add('nombre', 'text', array('attr' => array('class' => 'form-control', 'placeholder' => 'Nombre')))
+            ->add('apellido', 'text', array('attr' => array('class' => 'form-control', 'placeholder' => 'Apellido')))
+            ->add('telefono', 'text', array('attr' => array('class' => 'form-control', 'placeholder' => 'Teléfono', 'required' => false)))
+            ->add('celular', 'text', array('attr' => array('class' => 'form-control', 'placeholder' => 'Celular')))
+            ->add('direccion', 'text', array('attr' => array('class' => 'form-control', 'placeholder' => 'Dirección')))
+            ->add('email', 'email', array('attr' => array('class' => 'form-control', 'placeholder' => 'Email')))
+            ->add('password', 'password', array('attr' => array('class' => 'form-control', 'placeholder' => 'Clave')))
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver) {
+        $resolver->setDefaults(array(
+            'data_class' => 'TaxiAdmin\UsuarioBundle\Entity\Usuario'
+            ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName() {
+        return 'registro';
+    }
+}
