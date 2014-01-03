e11cadf39da4a94e2af2dd15bf6ff2fbdc900c0c
Bruno Viera <brunovierag@gmail.com>
2014-01-02 22:20:18 -0200

    modal refresh

--- src/TaxiAdmin/SitioBundle/Resources/public/js/app.js
+++ src/TaxiAdmin/SitioBundle/Resources/public/js/app.js
@@ -11,4 +11,9 @@ $(function(){
 	// agrego todos los tooltips
 	jQuery('.help-tooltip').tooltip();
 
+	// refresca el modal asi se utiliza uno solo por pagina 
+	$('body').on('hidden.bs.modal', '.modal', function () {
+		$(this).removeData('bs.modal');
+	});
+
 });
